e9312bbdf70f00d5aadc317a3f10635ddb7d9250
Slayorious <sgaweda@gmail.com>
2017-12-02 01:28:41 -0500

    Implemented meta character. Still need to compose lines of them.
    Signed-off-by: Slayorious <sgaweda@gmail.com>

--- library/window/model/VMChar.cpp
+++ library/window/model/VMChar.cpp
@@ -2,3 +2,18 @@
 // Created by Slayorious on 2017-12-01.
 //
 
+#include "VMChar.h"
+
+VMChar::VMChar(char c, int colour, unsigned char width) : c{c}, colour{colour}, width{width} {}
+
+char VMChar::getChar() { return c; }
+
+void VMChar::setChar(char newC) { c = newC; }
+
+int VMChar::getColour() { return colour; }
+
+void VMChar::setColour(int newColour) { colour = newColour; }
+
+unsigned char VMChar::getWidth() { return width; }
+
+void VMChar::setWidth(unsigned char newWidth) { width = newWidth; }
--- library/window/model/VMChar.h
+++ library/window/model/VMChar.h
@@ -5,4 +5,33 @@
 #ifndef VM_VMCHAR_H
 #define VM_VMCHAR_H
 
+#include <cstddef>
+
+class VMChar {
+	char c;
+	int colour;
+	unsigned char width;
+	//size_t startPos;
+
+public:
+	VMChar() = default;
+
+	VMChar(char c, int colour, unsigned char width);
+	//VMChar(char c, int colour, unsigned char width, size_t startPos);
+
+	char getChar();
+
+	void setChar(char newC);
+
+	int getColour();
+
+	void setColour(int newColour);
+
+	unsigned char getWidth();
+
+	void setWidth(unsigned char newWidth);
+	//size_t getStartPos();
+	//void setStartPos();
+};
+
 #endif //VM_VMCHAR_H
